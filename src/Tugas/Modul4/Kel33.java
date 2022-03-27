9e54eb90c32967a11ed174b7ace34ee0c4cf4b95
Ivan212-INH <nabiljapar@gmail.com>
2022-03-27 21:16:17 +0700

    Kode awal

---
+++ src/Tugas/Modul4/Kel33.java
@@ -0,0 +1,27 @@
+package Tugas.Modul4;
+import java.util.Scanner;
+
+public class Kel33 {
+    static String swit(int pilihan_menu) {
+        String[][] array = {{"air mineral Aqua", "soda Fanta", "Kopiko latte"}};
+        String hasil_akhir = null;
+        switch (pilihan_menu) {
+            case (1):
+                hasil_akhir = array[0][0];
+                break;
+            case (2):
+                hasil_akhir = array[0][1];
+                break;
+            case (3):
+                hasil_akhir = array[0][2];
+                break;
+        }return "Anda membeli " + hasil_akhir + " terima kasih telah membeli";
+    }
+        public static void main (String[] args){
+            menu awalan = new menu();
+            awalan.Selamat_datang();
+            Scanner input = new Scanner(System.in);
+            int pilihan = input.nextInt();
+            System.out.println(swit(pilihan));
+        }
+    }
---
+++ src/Tugas/Modul4/menu.java
@@ -0,0 +1,13 @@
+package Tugas.Modul4;
+
+public class menu{
+    static void Selamat_datang(){
+        System.out.println("Selamat datang di program vending machine kelompok 33");
+        System.out.println("Silahkan pilih menu yang anda ingin beli");
+        System.out.println("1. Air mineral Aqua");
+        System.out.println("2. Soda Fanta");
+        System.out.println("3. Kopiko Latte");
+        System.out.println("(1-3): ");
+    }
+}
+
